c0b580f27f432921502dc8d9f5dac89b5fdee9dd
Daniel Allen <dan@aeternagroup.net>
2026-01-11 18:06:11 +0000

    Update to schema version v4.57.0

---
+++ outputs.tf
@@ -0,0 +1,5 @@
+output "sentinel_data_connector_office_irms" {
+  description = "All sentinel_data_connector_office_irm resources"
+  value       = azurerm_sentinel_data_connector_office_irm.sentinel_data_connector_office_irms
+}
+
