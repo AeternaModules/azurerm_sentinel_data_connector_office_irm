74de77924e7670f0cb87d5da0b947da5901e0799
Daniel Allen <dan@aeternagroup.net>
2026-07-12 16:04:48 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
+output "sentinel_data_connector_office_irms_id" {
+  description = "Map of id values across all sentinel_data_connector_office_irms, keyed the same as var.sentinel_data_connector_office_irms"
+  value       = { for k, v in azurerm_sentinel_data_connector_office_irm.sentinel_data_connector_office_irms : k => v.id }
+}
 output "sentinel_data_connector_office_irms_log_analytics_workspace_id" {
   description = "Map of log_analytics_workspace_id values across all sentinel_data_connector_office_irms, keyed the same as var.sentinel_data_connector_office_irms"
   value       = { for k, v in azurerm_sentinel_data_connector_office_irm.sentinel_data_connector_office_irms : k => v.log_analytics_workspace_id }
